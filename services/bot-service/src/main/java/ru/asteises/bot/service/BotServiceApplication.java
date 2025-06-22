e647ed311411b9b4fb089411d18b7e50b1f9cbcb
f.nikitaev <f.nikitaev@bftcom.com>
2025-06-22 16:21:06 +0300

    init commit

---
+++ services/bot-service/src/main/java/ru/asteises/bot/service/BotServiceApplication.java
@@ -0,0 +1,11 @@
+package ru.asteises.bot.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BotServiceApplication.class, args);
+    }
+}
